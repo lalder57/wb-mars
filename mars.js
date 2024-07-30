ccf90167e2a2c53abe60b4f1412ffcfccc91ee59
Lauryn Thayne <lalder57@gmail.com>
2024-07-30 13:52:59 -0600

    Asked the user a question

--- mars.js
+++ mars.js
@@ -4,8 +4,23 @@
 // alert("All systems go!");
 // alert("Let's start!");
 
-const username = prompt("Hi there. What's your name?");
+// Ask username and give specific prompt
 
-alert(`Hi, ${username} -- Welcome to the Mars Adventure Game.`);
-alert("Yesterday you received an email from your good friend at NASA.");
-alert("They need someone to go to Mars this weekend, and YOU'VE been chosen!!");
+// const username = prompt("Hi there. What's your name?");
+
+// alert(`Hi, ${username} -- Welcome to the Mars Adventure Game.`);
+// alert("Yesterday you received an email from your good friend at NASA.");
+// alert("They need someone to go to Mars this weekend, and YOU'VE been chosen!!");
+
+// Ask the user a question
+
+const excited = prompt("Are you excited? Type Y or N.");
+
+if (excited === "Y") {
+  alert("I knew you'd say that. It's so cool that you're going to Mars!");
+}
+
+if (excited === "N") {
+  alert("Well, it's too late to back out now.");
+}
+
