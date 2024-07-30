b0fa53c9d87c683467c008315d357ad10cc45e8c
Lauryn Thayne <lalder57@gmail.com>
2024-07-30 13:43:28 -0600

    Added alerts and username prompts

---
+++ mars.js
@@ -0,0 +1,11 @@
+// alert("Hello!");
+// alert("Starting your Mars Adventure!");
+// alert("Booting up...");
+// alert("All systems go!");
+// alert("Let's start!");
+
+const username = prompt("Hi there. What's your name?");
+
+alert(`Hi, ${username} -- Welcome to the Mars Adventure Game.`);
+alert("Yesterday you received an email from your good friend at NASA.");
+alert("They need someone to go to Mars this weekend, and YOU'VE been chosen!!");
